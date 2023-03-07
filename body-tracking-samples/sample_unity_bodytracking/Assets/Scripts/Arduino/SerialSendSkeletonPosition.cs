3b7c398d3059807a787647822bc322dafeb2dc09
YH_hao_are_you <mlapenang1998@gmail.com>
2023-03-07 17:53:31 +0900

    Send position to Arduino

---
+++ body-tracking-samples/sample_unity_bodytracking/Assets/Scripts/Arduino/SerialSendSkeletonPosition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.Kinect.BodyTracking;
+using UnityEngine;
+
+public class SerialSendSkeletonPosition : MonoBehaviour
+{
+    [SerializeField] private SkeletonJointPosition skeletonJointPosition;
+    [SerializeField] private JointId[] joints = { JointId.Head, JointId.HandLeft, JointId.HandRight, JointId.Pelvis };
+    [SerializeField] private float sendInterval = 0.5f;
+    public SerialHandler serialHandler;
+
+    void Start()
+    {
+        InvokeRepeating("Send", 0, sendInterval);
+    }
+
+    private void Send()
+    {
+        serialHandler.Write(String.Join("|", joints.Select(jointId => 
+            FormatPosition(skeletonJointPosition.Get(jointId)))) + "\n");
+    }
+
+    private string FormatPosition(Vector3 pos)
+    {
+        return pos.x + "," + pos.y + "," + pos.z;
+    }
+}
---
+++ body-tracking-samples/sample_unity_bodytracking/Assets/Scripts/SkeletonJointPosition.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Kinect.BodyTracking;
+using UnityEngine;
+
+public class SkeletonJointPosition : MonoBehaviour
+{
+    [SerializeField] private Transform skeletonRoot;
+
+    private void Awake()
+    {
+        if (!skeletonRoot) skeletonRoot = transform;
+    }
+
+    public Vector3 Get(JointId jointId)
+    {
+        return transform.GetChild((int)jointId).localPosition;
+    }
+}
