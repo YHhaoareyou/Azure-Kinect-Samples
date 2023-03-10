e4f02c44f44d731be532d8bcc2775c667d5608c2
YH_hao_are_you <mlapenang1998@gmail.com>
2023-03-10 19:37:30 +0900

    Send joint rotation to Arduino

--- body-tracking-samples/sample_unity_bodytracking/Assets/Scripts/Arduino/SerialSendSkeletonPosition.cs
+++ body-tracking-samples/sample_unity_bodytracking/Assets/Scripts/Arduino/SerialSendSkeletonPosition.cs
@@ -7,20 +7,43 @@ using UnityEngine;
 
 public class SerialSendSkeletonPosition : MonoBehaviour
 {
-    [SerializeField] private SkeletonJointPosition skeletonJointPosition;
+    [SerializeField] private SkeletonJointPosition skeleton;
     [SerializeField] private JointId[] joints = { JointId.Head, JointId.HandLeft, JointId.HandRight, JointId.Pelvis };
     [SerializeField] private float sendInterval = 0.5f;
+
     public SerialHandler serialHandler;
+    private double wL;
+    private double wR;
+    private double wP;
 
     void Start()
     {
+        if (!skeleton) skeleton = FindObjectOfType<SkeletonJointPosition>();
         InvokeRepeating("Send", 0, sendInterval);
     }
 
     private void Send()
     {
-        serialHandler.Write(String.Join("|", joints.Select(jointId => 
-            FormatPosition(skeletonJointPosition.Get(jointId)))) + "\n");
+        CalculateJointRotationAngles();
+        serialHandler.Write(wL + "," + wR + "," + wP + "\n");
+    }
+
+    private void CalculateJointRotationAngles()
+    {
+        if (!skeleton) return;
+        
+        var frontVec = Vector3.Cross(skeleton.Get(JointId.ShoulderRight) - skeleton.Get(JointId.ShoulderLeft), Vector3.up).normalized;
+        var worldToLocalRot = Quaternion.FromToRotation(frontVec, Vector3.forward);
+        
+        wL = GetJointLocalRotationAngle(JointId.ClavicleLeft, JointId.WristLeft, worldToLocalRot);
+        wR = GetJointLocalRotationAngle(JointId.ClavicleRight, JointId.WristRight, worldToLocalRot);
+        wP = GetJointLocalRotationAngle(JointId.Pelvis, JointId.Head, worldToLocalRot);
+    }
+
+    private double GetJointLocalRotationAngle(JointId joint, JointId child, Quaternion worldToLocalRot)
+    {
+        var dir = worldToLocalRot * (skeleton.Get(child) - skeleton.Get(joint));
+        return Math.Atan2(dir.y, -dir.x) / 2 / Math.PI * 360;
     }
 
     private string FormatPosition(Vector3 pos)
